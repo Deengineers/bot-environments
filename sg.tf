eceb4aec603ebe8e39cab489dea9e0437a024e58
moabukar <mo-abukar10@hotmail.com>
2023-04-01 05:15:28 +0100

    update

--- sg.tf
+++ sg.tf
@@ -9,18 +9,6 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["149.86.9.168/32"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["82.25.136.113/32"]
-  }
 
   egress {
     from_port   = 0
