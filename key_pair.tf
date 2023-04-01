3500381c9d39cd07877fb90c392602abe5152586
moabukar <mo-abukar10@hotmail.com>
2023-04-01 18:49:08 +0100

    add key pair as a VAR

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,5 @@
 resource "aws_key_pair" "id_rsa" {
   key_name   = "id_rsa"
-  public_key = file("~/.ssh/id_rsa.pub")
+#   public_key = file("~/.ssh/id_rsa.pub") >> FOR LOCAL ONLY
+  public_key = var.public_key # for GHA
 }
---
+++ vars.tf
@@ -0,0 +1,4 @@
+variable "public_key" {
+  description = "The content of the public key to create the AWS key pair."
+  type        = string
+}
