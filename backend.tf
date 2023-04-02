6992a7bc0d5f58fa4c1f1d9ba71ffa6e40014b98
moabukar <mo-abukar10@hotmail.com>
2023-04-02 05:48:43 +0100

    move s3 backend to providers file

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tfstate"
-    key            = "terraform.tfstate"
-    region         = "us-east-1" # Change this to the region where your S3 bucket is located
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
--- providers.tf
+++ providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "tfstate"
+    key            = "terraform.tfstate"
+    region         = "us-east-1" # Change this to the region where your S3 bucket is located
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
 
 provider "aws" {
