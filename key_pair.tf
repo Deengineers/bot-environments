70a37da0bb549fc234c29419708a8b2a83f33f70
moabukar <mo-abukar10@hotmail.com>
2023-04-02 04:21:23 +0100

    tf fmt on keypair

--- key_pair.tf
+++ key_pair.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "id_rsa" {
-  key_name   = "id_rsa"
-#   public_key = file("~/.ssh/id_rsa.pub") >> FOR LOCAL ONLY
+  key_name = "id_rsa"
+  #   public_key = file("~/.ssh/id_rsa.pub") >> FOR LOCAL ONLY
   public_key = var.public_key # for GHA
 }
