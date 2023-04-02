c7abe5778ab6abdc940b9523bc2cb27c779538bb
moabukar <mo-abukar10@hotmail.com>
2023-04-02 05:56:39 +0100

    update backend region

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "tfstate"
     key            = "terraform.tfstate"
-    region         = "eu-west-3" 
+    region         = "us-east-1" 
     encrypt        = true
     dynamodb_table = "terraform_lock"
   }
